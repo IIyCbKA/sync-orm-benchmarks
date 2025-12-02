571b501d452a6016bec1e42ece58c5d34231fe45
Denis Lylov <denislylov2004@mail.ru>
2025-12-02 13:14:40 +0300

    new structure

--- benchmarks/sqlalchemy_bench/db_init/init.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-gunzip -c /docker-entrypoint-initdb.d/demo-20250901-3m.sql.gz | psql -U postgres
---
+++ benchmarks/sqlalchemy_bench/run-tests.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cd $(dirname $0)
+
+# Test 1 → Insert
+python -m sync.test_1
