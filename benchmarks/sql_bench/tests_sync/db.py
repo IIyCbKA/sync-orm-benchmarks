f3c05e9f4a6f55ede448aee69c8cffb3c9b5aff8
IIyCbKA <alex.chernobrovenko2005@yandex.ru>
2026-01-11 02:41:39 +0300

    feat(sql): add ConnectionPool to sync sql solution

--- benchmarks/sql_bench/tests_sync/db.py
+++ benchmarks/sql_bench/tests_sync/db.py
@@ -1,21 +1,23 @@
-import os
-import psycopg
 from dotenv import load_dotenv
-
+from psycopg_pool import ConnectionPool
+import os
 
 load_dotenv()
+
 DB_USER = os.getenv("POSTGRES_USER")
 DB_PASS = os.getenv("POSTGRES_PASSWORD")
 DB_HOST = os.getenv("POSTGRES_HOST")
 DB_NAME = os.getenv("POSTGRES_DB")
 DB_PORT = os.getenv("POSTGRES_PORT")
 
-def get_connection() -> psycopg.Connection:
-    return psycopg.connect(
-        host=DB_HOST,
-        port=DB_PORT,
-        dbname=DB_NAME,
-        user=DB_USER,
-        password=DB_PASS,
-    )
+pool = ConnectionPool(
+    conninfo=f'dbname={DB_NAME} user={DB_USER} password={DB_PASS} host={DB_HOST} port={DB_PORT}',
+    min_size=1,
+    max_size=25,
+    timeout=30,
+    max_lifetime=3600,
+    max_idle=600,
+)
 
+def get_connection():
+    return pool.getconn()
